aa08aa169372d6ab55bcb41876141668f5d9c78f
Kustaa Nyholm <kustaa.nyholm@planmeca.com>
2014-10-20 00:15:08 +0300

    Javadoc comment updats

--- src/purejavahidapi/HidDevice.java
+++ src/purejavahidapi/HidDevice.java
@@ -30,21 +30,154 @@
 package purejavahidapi;
 
 import java.util.List;
+/**
+ * Instances of HidDevice represent a single physical USB HID device that has been
+ * opened for communication. 
+ * <p>
+ * If the {@link HidDevice.close()} has been called for an object then no more
+ * call should be made to any of the methods of that object and attempts to do
+ * that will result in IllegalState exception being thrown.
+ * <p>
+ * Each HidDevice instance creates an internal background thread to perform some
+ * work in the background. This thread may keep an application that uses PureJavaHidApi
+ * from exiting if the device is not closed before the application tries to exit.
+ * <p>
+ * @author nyholku
+ *
+ */
 
 public interface HidDevice {
 
+	/**
+	 * This method sets the input report listener for this device.
+	 * <p>
+	 * There can be only one input report listener at a time but it is
+	 * possible to re-set the listener.
+	 * <p>
+	 * Setting the input report listener is the only way to receive
+	 * input reports from the device.
+	 * <p>
+	 * The {@link InputReportListener#onInputReport} method is called
+	 * from an internal (to the device instance) background thread and
+	 * it is expected that the listener will process the data as quickly
+	 * as possible and not do any length process in the callback.
+	 * <p>
+	 * 
+	 * @param listener the listener object or null to un-set
+	 */
 	public void setInputReportListener(InputReportListener listener);
-
+	/**
+	 * This method sends an output report to the device.
+	 * <p>
+	 * If numbered reports are used (see USB HID specification for
+	 * explanation about numbered reports) the reportID needs to
+	 * be specified otherwise pass zero there.
+	 * <p>
+	 * This method may or may not block. 
+	 * <p>
+	 * The method returning is no guarantee that the data has been physically
+	 * transmitted from the host to the device.
+	 * <p>
+	 * The method returns the actual number of bytes successfully scheduled
+	 * to be sent to the device. 
+	 * <p>
+	 * As of this writing it is unclear under
+	 * what circumstances something else than 'length' number of bytes
+	 * could be returned as well as what happens if the report length
+	 * does not match what the device expects.
+	 * <p>
+	 * 
+	 * @param reportID the report number if numbered reports are used else pass 0
+	 * @param data a byte array containing the data to be sent
+	 * @param length the number of bytes to send from the data array 
+	 * @return number bytes actually sent or -1 if the send failed
+	 */
+	 
 	public int setOutputReport(byte reportID,byte[] data, int length);
+	/**
+	 * This method sends a feature report to the device.
+	 * <p>
+	 * See USB HID specification to learn more about feature reports.
+	 * <p>
+	 * This method may or may not block. 
+	 * <p>
+	 * The method returning is no guarantee that the data has been physically
+	 * transmitted from the host to the device.
+	 * <p>
+	 * The method returns the actual number of bytes successfully scheduled
+	 * to be sent to the device. 
+	 * <p>
+	 * As of this writing it is unclear under
+	 * what circumstances something else than 'length' number of bytes
+	 * could be returned as well as what happens if the report length
+	 * does not match what the device expects.
+	 * <p>
+	 * 
+	 * @param data a byte array containing the data to be sent
+	 * @param length the number of bytes to send from the data array 
+	 * @return number bytes actually sent or -1 if the call failed
+	 * 
+	 */
 
 	public int setFeatureReport(byte[] data, int length);
 
+	/**
+	 * This method reads a feature report from the device.
+	 * <p>
+	 * See USB HID specification to learn more about feature reports.
+	 * <p>
+	 * This method blocks until the 'length' number of bytes has
+	 * been received to the 'data' buffer or the device is closed.
+	 * <p>
+	 * This method returns the number of bytes actually received
+	 * or -1 in case there was error in communicating with the
+	 * device.
+	 * <p>
+	 * As of this writing it is unclear under
+	 * what circumstances something else than 'length' number of bytes
+	 * could be returned as well as what happens if the report length
+	 * does not match what the device expects.
+	 * <p>
+	 * 
+	 * @param data the byte array to receive the data
+	 * @param length the size of the feature report
+	 * @return the number of bytes actually received or -1 if the call failed
+	 */
 	public int getFeatureReport(byte[] data, int length);
 
+	/**
+	 * This method sets the device removal listener.
+	 * <p>
+	 * The {@link DeviceRemovalListener#onDeviceRemoval} method of the
+	 * device listener object will be called if and when the device is 
+	 * unexpectedly un-plugged.
+	 * 
+	 * There can be only one device removal listener at a time but it is
+	 * possible to re-set the listener.
+	 * <p>
+	 * @param listener the device removal listener object or null to un-set
+	 */
 	public void setDeviceRemovalListener(DeviceRemovalListener listener);
 
+	/**
+	 * This method returns the same info that the {@link PureJavaHidApi#enumerateDevices()
+	 * would return for this device.
+	 * 
+	 * @return the device info object
+	 */
 	public HidDeviceInfo getHidDeviceInfo();
 
+	/**
+	 * This method closes the access to the device.
+	 * 
+	 * After calling close no further method calls to this object must be done,
+	 * otherwise an IllegalStateException will be generated.
+	 * 
+	 * This call may block indefinitely if the device stops sending reports.
+	 * (Hopefully in the future the close method will not block under any
+	 * circumstances).
+	 * 
+	 */
 	public void close();
 
 	//int setOutputReport(byte reportID, byte[] data, int length);
