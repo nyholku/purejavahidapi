a63d356ef14f7109c69de0c070f386c1fc4c8c2b
Kustaa Nyholm <kustaa.nyholm@planmeca.com>
2014-10-19 13:16:43 +0300

    Test

--- update-javadoc.sh
+++ update-javadoc.sh
@@ -2,8 +2,7 @@
 # Push javadoc files to a website hosted by github <http://pages.github.com/>.
 # Before executing this script, generate the javadoc files into build/docs/javadoc/.
 git checkout master || exit $?
-javadoc -d build/javadoc  -sourcepath src purejavahidapi 
-echo here
+javadoc -q -d build/javadoc  -sourcepath src purejavahidapi 
 git checkout gh-pages || exit $?
 
 # Clear out the old files:
