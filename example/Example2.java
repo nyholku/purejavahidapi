a6877ef773396277fba8c674fe6f5ce3bb7d211f
Kustaa Nyholm <kustaa.nyholm@planmeca.com>
2014-10-19 22:12:07 +0300

    Swapped example project names

--- example/Example2.java
+++ example/Example2.java
@@ -7,15 +7,26 @@ public class Example2 {
 	public static void main(String[] args) {
 		try {
 			List<HidDeviceInfo> devList = PureJavaHidApi.enumerateDevices();
-			HidDevice dev = null;
+			HidDeviceInfo devInfo = null;
 			for (HidDeviceInfo info : devList) {
-				System.out.printf("VID = 0x%04X PID = 0x%04X Manufacturer = %s Product = %s Path = %s\n", //
-						info.getVendorId(), //
-						info.getProductId(), //
-						info.getManufacturerString(), //
-						info.getProductString(), //
-						info.getPath());
-
+				if (info.getVendorId() == 0x0810 && info.getProductId() == 0x0005) {
+					devInfo = info;
+					break;
+				}
+			}
+			if (devInfo == null)
+				System.err.println("device not found");
+			else {
+				HidDevice dev=PureJavaHidApi.openDevice(devInfo.getPath());
+				dev.setInputReportListener(new InputReportListener() {
+					@Override
+					public void onInputReport(HidDevice source, byte Id, byte[] data, int len) {
+						System.out.printf("onInputReport: id %d len %d data ", Id, len);
+						for (int i = 0; i < len; i++)
+							System.out.printf("%02X ", data[i]);
+						System.out.println();
+					}
+				});
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
