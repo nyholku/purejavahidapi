dbdaf2dce11f230364a460d8c9ac4d507bf909f5
Mike Vogt <mike.vogt@iwes.fraunhofer.de>
2015-12-03 11:39:02 +0100

    changed the path of all sources to be maven compatible and added a pom.xml which will build a maven compatible lib.

---
+++ src/main/java/com/sparetimelabs/purejavahidapi/shared/Frontend.java
@@ -0,0 +1,7 @@
+package com.sparetimelabs.purejavahidapi.shared;
+
+import com.sparetimelabs.purejavahidapi.HidDevice;
+
+public interface Frontend {
+	void closeDevice(HidDevice device);
+}
--- src/purejavahidapi/shared/Frontend.java
+++
@@ -1,7 +0,0 @@
-package purejavahidapi.shared;
-
-import purejavahidapi.HidDevice;
-
-public interface Frontend {
-	void closeDevice(HidDevice device);
-}
