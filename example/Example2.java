1e61d0e883b9b259e8ac1057a6165a329fc5400c
Kustaa Nyholm <kustaa.nyholm@planmeca.com>
2016-06-30 06:27:36 +0300

    Added example code to read feature reports

--- example/Example2.java
+++ example/Example2.java
@@ -6,16 +6,13 @@ public class Example2 {
 
 	public static void main(String[] args) {
 		try {
-			
-			
-			
-			
+
 			//System.exit(0);
-			
+
 			List<HidDeviceInfo> devList = PureJavaHidApi.enumerateDevices();
 			HidDeviceInfo devInfo = null;
 			for (HidDeviceInfo info : devList) {
-				if (info.getVendorId() == (short) 0x0810 && info.getProductId() == (short) 0x0005) {
+				if (info.getVendorId() == (short) 0x16C0 && info.getProductId() == (short) 0x05DF) {
 					devInfo = info;
 					break;
 				}
@@ -39,11 +36,29 @@ public class Example2 {
 						System.out.println();
 					}
 				});
+
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						while (true) {
+							byte[] data = new byte[132];
+							data[0] = 1;
+							int len = 0;
+							if (((len = dev.getFeatureReport(data, data.length)) >= 0)) {
+								int Id=data[0];
+								System.out.printf("getFeatureReport: id %d len %d data ", Id, len);
+								for (int i = 0; i < data.length; i++)
+									System.out.printf("%02X ", data[i]);
+								System.out.println();
+							}
+						}
+
+					}
+				}).run();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 	}
-
 }
