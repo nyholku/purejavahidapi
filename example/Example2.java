4388fde3328ef5e8eb7d13ef82c91a79741f0b0a
Kustaa Nyholm <kustaa.nyholm@planmeca.com>
2016-09-24 08:19:48 +0300

    Updated to comply with the API change

--- example/Example2.java
+++ example/Example2.java
@@ -3,60 +3,81 @@ import java.util.List;
 import purejavahidapi.*;
 
 public class Example2 {
+	volatile static boolean deviceOpen = false;
 
 	public static void main(String[] args) {
 		try {
 
-			//System.exit(0);
+			while (true) {
+				// System.exit(0);
+				HidDeviceInfo devInfo = null;
+				if (!deviceOpen) {
+					System.out.println("scanning");
+					List<HidDeviceInfo> devList = PureJavaHidApi.enumerateDevices();
+					for (HidDeviceInfo info : devList) {
+						// if (info.getVendorId() == (short) 0x16C0 &&
+						// info.getProductId() == (short) 0x05DF) {
+						//if (info.getVendorId() == (short) 0x16C0 && info.getProductId() == (short) 0x0a99) {
+							if (info.getVendorId() == (short) 0x16D0 && info.getProductId() == (short) 0x044B) {
+							devInfo = info;
+							break;
+						}
+					}
+					if (devInfo == null) {
+						System.out.println("device not found");
+						Thread.sleep(1000);
+					} else {
+						System.out.println("device found");
+						if (true) {
+							deviceOpen = true;
+							final HidDevice dev = PureJavaHidApi.openDevice(devInfo);
 
-			List<HidDeviceInfo> devList = PureJavaHidApi.enumerateDevices();
-			HidDeviceInfo devInfo = null;
-			for (HidDeviceInfo info : devList) {
-				if (info.getVendorId() == (short) 0x16C0 && info.getProductId() == (short) 0x05DF) {
-					devInfo = info;
-					break;
+							dev.setDeviceRemovalListener(new DeviceRemovalListener() {
+								@Override
+								public void onDeviceRemoval(HidDevice source) {
+									System.out.println("device removed");
+									deviceOpen = false;
+
+								}
+							});
+							dev.setInputReportListener(new InputReportListener() {
+								@Override
+								public void onInputReport(HidDevice source, byte Id, byte[] data, int len) {
+									System.out.printf("onInputReport: id %d len %d data ", Id, len);
+									for (int i = 0; i < len; i++)
+										System.out.printf("%02X ", data[i]);
+									System.out.println();
+								}
+							});
+
+							new Thread(new Runnable() {
+								@Override
+								public void run() {
+									while (true) {
+										byte[] data = new byte[132];
+										data[0] = 1;
+										int len = 0;
+										if (((len = dev.getFeatureReport(data, data.length)) >= 0) && true) {
+											int Id = data[0];
+											System.out.printf("getFeatureReport: id %d len %d data ", Id, len);
+											for (int i = 0; i < data.length; i++)
+												System.out.printf("%02X ", data[i]);
+											System.out.println();
+										}
+
+									}
+
+								}
+							}).start();
+
+							Thread.sleep(2000);
+							//dev.close();
+							//deviceOpen = false;
+						}
+					}
 				}
 			}
-			if (devInfo == null)
-				System.err.println("device not found");
-			else {
-				final HidDevice dev = PureJavaHidApi.openDevice(devInfo.getPath());
-				dev.setDeviceRemovalListener(new DeviceRemovalListener() {
-					@Override
-					public void onDeviceRemoval(HidDevice source) {
-						System.out.println("onDeviceRemoval");
-					}
-				});
-				dev.setInputReportListener(new InputReportListener() {
-					@Override
-					public void onInputReport(HidDevice source, byte Id, byte[] data, int len) {
-						System.out.printf("onInputReport: id %d len %d data ", Id, len);
-						for (int i = 0; i < len; i++)
-							System.out.printf("%02X ", data[i]);
-						System.out.println();
-					}
-				});
-
-				new Thread(new Runnable() {
-					@Override
-					public void run() {
-						while (true) {
-							byte[] data = new byte[132];
-							data[0] = 1;
-							int len = 0;
-							if (((len = dev.getFeatureReport(data, data.length)) >= 0)) {
-								int Id=data[0];
-								System.out.printf("getFeatureReport: id %d len %d data ", Id, len);
-								for (int i = 0; i < data.length; i++)
-									System.out.printf("%02X ", data[i]);
-								System.out.println();
-							}
-						}
-
-					}
-				}).run();
-			}
-		} catch (Exception e) {
+		} catch (Throwable e) {
 			e.printStackTrace();
 		}
 
