eab7d52a1d8b259817e2144fe13cb317b003c76a
Nicholas Saney <nsaney@gmail.com>
2018-04-27 02:21:11 -0400

    Updated copyright notice.

--- src/purejavahidapi/shared/DataDump.java
+++ src/purejavahidapi/shared/DataDump.java
@@ -1,14 +1,15 @@
 /*
  * Copyright (c) 2014, Kustaa Nyholm / SpareTimeLabs
+ * Copyright (c) 2018, Nicholas Saney / Chairosoft
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
  *
- * Redistributions of source code must retain the above copyright notice, this list
+ * Redistributions of source code must retain the above copyright notices, this list
  * of conditions and the following disclaimer.
  *
- * Redistributions in binary form must reproduce the above copyright notice, this
+ * Redistributions in binary form must reproduce the above copyright notices, this
  * list of conditions and the following disclaimer in the documentation and/or other
  * materials provided with the distribution.
  *
