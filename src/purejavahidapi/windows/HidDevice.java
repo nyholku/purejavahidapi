b1dd150082911e64e46b9b1a78dee66675f04297
Adam Mitz <mitza@objectcomputing.com>
2019-01-31 15:39:14 -0600

    windows HidDevice: simplified ReadFile/GetOverlappedResult calls

--- src/purejavahidapi/windows/HidDevice.java
+++ src/purejavahidapi/windows/HidDevice.java
@@ -47,14 +47,9 @@ import static purejavahidapi.windows.Kernel32Library.GetLastError;
 import static purejavahidapi.windows.Kernel32Library.GetOverlappedResult;
 import static purejavahidapi.windows.Kernel32Library.IOCTL_HID_GET_FEATURE;
 import static purejavahidapi.windows.Kernel32Library.ReadFile;
-import static purejavahidapi.windows.Kernel32Library.ResetEvent;
 import static purejavahidapi.windows.Kernel32Library.WriteFile;
 import static purejavahidapi.windows.SetupApiLibrary.HIDP_STATUS_SUCCESS;
 import static purejavahidapi.windows.WinDef.INVALID_HANDLE_VALUE;
-
-import java.time.Duration;
-import java.time.Instant;
-import java.util.Arrays;
 
 import com.sun.jna.Memory;
 import com.sun.jna.NativeLong;
@@ -68,8 +63,6 @@ import purejavahidapi.windows.WinDef.HANDLE;
 import purejavahidapi.windows.WinDef.OVERLAPPED;
 
 public class HidDevice extends purejavahidapi.HidDevice {
-	protected static final long HID_INPUTREPORT_GETOVERLAPPED_DELAY_MS = 20L;
-	
 	private WindowsBackend m_Backend;
 	private HANDLE m_Handle;
 	private int m_OutputReportLength;
@@ -77,12 +70,6 @@ public class HidDevice extends purejavahidapi.HidDevice {
 	private OVERLAPPED m_OutputReportOverlapped;
 	private int[] m_OutputReportBytesWritten;
 	private int m_InputReportLength;
-	private OVERLAPPED m_InputReportOverlapped = new OVERLAPPED();
-	private Memory m_InputReportMemory;
-	private byte[] m_InputReportBytes;
-	private int[] m_InputReportBytesRead = {
-			0
-	};
 	private Thread m_Thread;
 	private SyncPoint m_SyncStart;
 	private SyncPoint m_SyncShutdown;
@@ -123,17 +110,6 @@ public class HidDevice extends purejavahidapi.HidDevice {
 		};
 
 		m_InputReportLength = caps.InputReportByteLength;
-		m_InputReportOverlapped = new OVERLAPPED();
-		// auto event for overlapped
-		m_InputReportOverlapped.hEvent = CreateEvent(null, false, false, null);
-		if (m_InputReportLength > 0) {
-			m_InputReportMemory = new Memory(m_InputReportLength);
-			m_InputReportBytes = new byte[m_InputReportLength];
-		}
-		m_InputReportBytesRead = new int[] {
-				0
-		};
-
 		HidD_FreePreparsedData(ppd[0]);
 
 		m_SyncStart = new SyncPoint(2);
@@ -270,20 +246,25 @@ public class HidDevice extends purejavahidapi.HidDevice {
 
 	private void runReadOnBackground() {
 		m_SyncStart.waitAndSync();
-		while (!m_StopThread) {
-			m_InputReportBytesRead[0] = 0;
-			if (!ResetEvent(m_InputReportOverlapped.hEvent)) {
-				System.err.println("ResetEvent failed with GetLastError()==" + GetLastError());
-			}
 
-			// In Windows ReadFile() from a HID device Windows expects us to attempt to read
-			// as much bytes as there are
-			// in the longest report plus one for the report number (even if not used) and
-			// the data is always
-			// preceded with the report number (even if not used in case of which it is
-			// zero)
-			if (!ReadFile(m_Handle, m_InputReportMemory, m_InputReportLength, m_InputReportBytesRead,
-					m_InputReportOverlapped)) {
+		int[] numBytesRead = { 0 };
+		OVERLAPPED overlapped = new OVERLAPPED();
+		overlapped.hEvent = CreateEvent(null, true, true, null);
+		Memory readBuffer = new Memory(m_InputReportLength);
+
+		while (!m_StopThread) {
+			numBytesRead[0] = 0;
+			overlapped.Internal = null;
+			overlapped.InternalHigh = null;
+			overlapped.Offset = 0;
+			overlapped.OffsetHigh = 0;
+
+			// In Windows ReadFile() from a HID device Windows expects us to
+			// attempt to read as much many as there are in the longest report
+			// plus one for the report number (even if not used) and the data
+			// is always preceded with the report number (even if not used in
+			// case of which it is zero)
+			if (!ReadFile(m_Handle, readBuffer, m_InputReportLength, numBytesRead, overlapped)) {
 				if (GetLastError() == ERROR_DEVICE_NOT_CONNECTED)
 					break; // early exit if the device disappears
 				if (GetLastError() != ERROR_IO_PENDING) {
@@ -291,7 +272,7 @@ public class HidDevice extends purejavahidapi.HidDevice {
 					System.err.println("ReadFile failed with GetLastError()==" + GetLastError());
 				}
 
-				if (!GetOverlappedResult(m_Handle, m_InputReportOverlapped, m_InputReportBytesRead, true/* wait */)) {
+				if (!GetOverlappedResult(m_Handle, overlapped, numBytesRead, true/* wait */)) {
 					if (GetLastError() == ERROR_DEVICE_NOT_CONNECTED)
 						break; // early exit if the device disappears
 					if (m_StopThread && GetLastError() == ERROR_OPERATION_ABORTED)
@@ -300,75 +281,23 @@ public class HidDevice extends purejavahidapi.HidDevice {
 				}
 			}
 
-			processInputReport();
-
-			// in case the second... GetOverlappedResult failed because of disconnection
-			// or close, shutdown...
-			if (GetLastError() == ERROR_DEVICE_NOT_CONNECTED)
-				break; // early exit if the device disappears
-			if (m_StopThread && GetLastError() == ERROR_OPERATION_ABORTED)
-				break; // on close
+			processInputReport(readBuffer, numBytesRead[0]);
 		}
+		
+		CloseHandle(overlapped.hEvent);
 		m_SyncShutdown.waitAndSync();
 	}
 
-	private void processInputReport() {
-		byte lastReportID = -1;
-		byte[] lastDataBuff = null;
-		while (true) {
-			if (m_InputReportBytesRead[0] > 0) {
-				byte reportID = m_InputReportMemory.getByte(0);
-				int len = m_InputReportBytesRead[0] - 1;
-				m_InputReportMemory.read(1, m_InputReportBytes, 0, len);
-				// Need to copy because the buffer sometime change behind, and we keep a copy
-				byte[] dataBuff = Arrays.copyOf(m_InputReportBytes, len);
-				if (lastDataBuff != null && Arrays.equals(dataBuff, lastDataBuff) && lastReportID == reportID) {
-					// report is same, go back to ReadFile
-					break;
-				} else {
-					Instant start = Instant.now();
-					if (m_InputReportListener != null) {
-						m_InputReportListener.onInputReport(this, reportID, dataBuff, len);
-					}
-					delayAfterListener(start);
-					lastDataBuff = dataBuff;
-					lastReportID = reportID;
-				}
-			} else {
-				// report is empty, go back to ReadFile
-				break;
+	private void processInputReport(Memory readBuffer, int numBytesRead) {
+		if (numBytesRead > 0) {
+			byte reportID = readBuffer.getByte(0);
+			int len = numBytesRead - 1;
+			byte[] inputReport = new byte[len];
+			readBuffer.read(1, inputReport, 0, len);
+			if (m_InputReportListener != null) {
+				m_InputReportListener.onInputReport(this, reportID, inputReport, len);
 			}
-			// Strangely on a Ledger Nano S, sometime the Report changes just after
-			// GetOverlappedResult
-			// the second GetOverlappedResult will give a new result
-			// but afterward it will always give the same result, and will do that endlessly
-			m_InputReportBytesRead[0] = 0;
-			if (!GetOverlappedResult(m_Handle, m_InputReportOverlapped, m_InputReportBytesRead, true)) {
-				// abnormal error, ignoring, go back to ReadFile
-				break;
-			}
-			// report is changed, so loop again to send it to the listener,
-			// and try to see if a new one appears
 		}
 	}
 
-	private void delayAfterListener(Instant start) {
-		if (HID_INPUTREPORT_GETOVERLAPPED_DELAY_MS > 0) {
-			long remains = Duration
-					.between(Instant.now(), start.plusMillis(HID_INPUTREPORT_GETOVERLAPPED_DELAY_MS))
-					.toMillis();
-			if (remains <= 0)
-				remains = 1;
-			try {
-				// tested with Ledger nano S
-				// without this delay, the second HID InputReport is missed
-				// with delay>1ms it often works one time, but not 2
-				// with delay <11ms when looping it often miss the 2nd frame over 3
-				// with 20ms it seems OK.
-				Thread.sleep(remains);
-			} catch (InterruptedException e) {
-				Thread.currentThread().interrupt();
-			}
-		}
-	}
 }
